f6541f0acd66b2c9ce7a194246d76f1e0129956d
putzoludaniele3 <git config --daniele.putzolu.0103@gmail.com>
2024-10-21 14:53:55 +0200

    fix bottoni, href, img

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +11,20 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  dowloadCV(){
+    const pdfUrl = 'assets/file/DANIELE-PUTZOLU-CV.pdf';
+    const pdfName = 'DANIELE-PUTZOLU-CV.pdf'
+
+
+    const a = document.createElement('a');
+    a.href = pdfUrl;
+    a.download = pdfName;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
   }
 
 }
